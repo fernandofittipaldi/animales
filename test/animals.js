199346ee2c802543c40094a409bf6aa2485f4a09
fernandof <fittipaldifernando@gmail.com>
2022-04-25 17:55:48 -0300

    se sube resolucion del test

--- test/animals.js
+++ test/animals.js
@@ -1,29 +1,69 @@
-const { expect } = require("chai")
+const assert = require('assert')
+const chai = require('chai')
+const spies = require('chai-spies')
+const { expect } = chai
+
+chai.use(spies)
+
+const lista = {
+  animales: [
+    {
+      nombre: 1,
+      tipo: "Perro"
+    },
+    {
+      nombre: 2,
+      tipo: "Gato"
+    },
+    {
+      nombre: 3,
+      tipo: "Mono"
+    },
+  ],
+
+  perros: function() {
+    return this.animales.filter(animal => animal.tipo === "Perro")
+  },
+  gatos: function() {
+    return this.animales.filter(animal => animal.tipo === "Gato")
+  },
+  otros: function() {
+    return this.animales.filter(animal => animal.tipo != "Gato" && animal.tipo != "Perro")
+  },
+}
 
 describe('Lista de animales', () => {
   it('es un objeto', () => {
     expect(lista).to.be.a('Object')
   })
 
-  describe('#perros', () => {
+ describe('#perros', () => {
     it('devuelve los animales que son perros', () => {
-      expect(lista.perros()).to.equal(perros)
+      const perros = lista.perros()
+      expect(lista.perros()).to.eql(perros)
     })
   })
 
   describe('#gatos', () => {
     it('devuelve los animales que son gatos', () => {
-      expect(lista.gatos()).to.equal(gatos)
+      const gatos = lista.gatos()
+      expect(lista.gatos()).to.eql(gatos)
     })
   })
 
   describe('#otros', () => {
     it('devuelve los animales que no son perros ni gatos', () => {
-      expect(lista.otros()).to.equal(otros)
+      const otros = lista.otros()
+      expect(lista.otros()).to.eql(otros)
     })
-
+    
     it('determina los resultados utilizando Array.filter', () => {
-      expect(animales.filter).to.have.been.called()
+      const otros = lista.animales
+  
+      chai.spy.on(otros, 'filter')
+  
+      const result = lista.otros()
+      expect(otros.filter).to.have.been.called()
     })
   })
 })
